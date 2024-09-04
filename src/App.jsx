a7d3f9d0ff98905d5cd1f4a498fcfd8c28521fd7
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-09-05 04:58:06 +0530

    "Might have bloated it a bit,  Creates cards"

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 import React from 'react';
 import Navbar from './navbar'; // Adjust path as needed
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Optional, for route navigation
+import BottomActionsCard from './cards';
 
 const App = () => {
   return (
+    <>
     <Router>
       <div>
         <Navbar />
       </div>
+      <div><BottomActionsCard/></div>
     </Router>
+    </>
   );
 };
 
---
+++ src/cards.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardActions from "@mui/joy/CardActions";
+import Typography from "@mui/joy/Typography";
+
+export default function BottomActionsCard() {
+  let details = "Well Hello there for now";
+  let user = "Test for now";
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        width: 320,
+        // to make the card resizable
+        overflow: "auto",
+        resize: "horizontal",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      ></Box>
+      <CardContent>
+        <Typography level="title-lg">{user}</Typography>
+        <Typography level="body-sm">{details}</Typography>
+      </CardContent>
+      <CardActions buttonFlex="0 1 120px">
+        <Button variant="outlined" color="neutral">
+          View
+        </Button>
+        <Button variant="solid" color="primary">
+          Join
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
